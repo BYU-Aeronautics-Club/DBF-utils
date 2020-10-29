a7cbe80e6ee1b88f045f398e3b3cffc205631d4b
Judd Mehr <juddmehr@gmail.com>
2020-10-29 15:00:22 -0600

    fix typo

--- reports/2020/sensitivityplot.jl
+++ reports/2020/sensitivityplot.jl
@@ -12,14 +12,14 @@ loadlength = makeline(0.79,x,b)
 loadunload = makeline(1.0,x,b)
 m1 = makeline(0.0,x,b)
 
-x.*100
+x *= 100
 
 figure(figsize=(2.4,2))
 clf()
 plot(x,speed,label="Speed","C0")
 plot(x,loadunload,label="Load/Unload Time","C1")
 plot(x,weight,label="Sensor Weight","C3",linewidth=1.5)
-plot(x,loadlength,label="Sensor Lenght","--C1")
+plot(x,loadlength,label="Sensor Length","--C1")
 plot(x,numcontainers,label="Num Containers","C2")
 plot(x,m1,label="M1 Success","--C2")
 legend()
